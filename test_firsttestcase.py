bc8ed34d26e365ca41bad959afb986d81dfc1e91
Snagaveni <43534551+Snagaveni@users.noreply.github.com>
2025-02-04 21:34:53 +0530

    test file

---
+++ test_firsttestcase.py
@@ -0,0 +1,16 @@
+from appium import webdriver
+import pytest
+from appium.options.common import AppiumOptions
+
+
+def test_mobilefirstcase():
+    desired_caps = dict(
+        platformName='Android',
+        deviceName='Android',
+        browserName='Chrome'
+    )
+
+    driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',options=AppiumOptions().load_capabilities(desired_caps))
+    driver.get("http://google.com")
+    print(driver.title)
+
